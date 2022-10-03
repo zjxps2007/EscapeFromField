7a32261f39303c313a40f8a00dd5da94c6ced778
joinho <zjxps2007@gmail.com>
2022-10-03 23:27:16 +0900

    file update

---
+++ EscapeFromFielld/Assets/Resources/UsrLog.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class UsrLog : MonoBehaviour
+{
+    string user_log;
+    string log_path;
+
+    // Use this for initialization
+    void Start()
+    {
+        user_log = System.DateTime.Now.ToString("yyyy-MM-dd, hh:mm:ss") + " (" +  SystemInfo.deviceName + ", " + SystemInfo.deviceUniqueIdentifier + ")\n";
+
+#if UNITY_EDITOR
+		log_path = Path.Combine(Application.dataPath + "/Resources", "user_log.json");
+
+#elif UNITY_IOS
+		log_path = Path.Combine(Application.temporaryCachePath + "/Raw", "user_log.json");
+
+#elif UNITY_ANDROID
+		log_path = Path.Combine(Application.temporaryCachePath, "user_log.json");
+#endif
+
+        FileStream file = new FileStream(log_path, FileMode.Append, FileAccess.Write);
+        file.Flush();
+        StreamWriter sw = new StreamWriter(file);
+        sw.Write(user_log);
+
+        sw.Close();
+        file.Close();
+    }
+}

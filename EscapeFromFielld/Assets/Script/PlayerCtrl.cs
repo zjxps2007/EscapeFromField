0bbe33eb68b43151a6ea67a8cab44383fe4282ed
joinho <zjxps2007@gmail.com>
2022-10-23 21:40:46 +0900

    add to move_code

--- EscapeFromFielld/Assets/Script/PlayerCtrl.cs
+++ EscapeFromFielld/Assets/Script/PlayerCtrl.cs
@@ -1,9 +1,18 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerCtrl : MonoBehaviour
 {
+    private float speed = 1f;
+
+    private Animator anim;
+
+    private Vector3 movDir;
+
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +22,38 @@ public class PlayerCtrl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Move();
+        Walk();
         
     }
+
+    void Walk()
+    {
+        movDir = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        if (movDir != Vector3.zero)
+        {
+            anim.SetBool("IsWalk",true);
+        }
+        else
+        {
+            anim.SetBool("IsWalk", false);
+        }
+
+        if (Input.GetKey(KeyCode.LeftShift) && movDir != Vector3.zero)
+        {
+            anim.SetBool("IsRun", true);
+        }
+        else
+        {
+            anim.SetBool("IsRun", false);
+        }
+    }
+
+    void Move()
+    {
+        transform.Translate(Vector3.forward * Input.GetAxis("Vertical") * speed * Time.deltaTime);
+        transform.Translate(Vector3.right * Input.GetAxis("Horizontal") * speed * Time.deltaTime);
+    }
+    
+    
 }

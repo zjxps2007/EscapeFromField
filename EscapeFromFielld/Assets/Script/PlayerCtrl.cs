fc592c9a1155f1d2faec2eef32d39b647f2d7ba9
zjxps2007 <zjxps2007@gmail.com>
2022-10-24 14:36:44 +0900

    Update

--- EscapeFromFielld/Assets/Script/PlayerCtrl.cs
+++ EscapeFromFielld/Assets/Script/PlayerCtrl.cs
@@ -1,12 +1,12 @@
+using System;
 using UnityEngine;
 
 public class PlayerCtrl : MonoBehaviour
 {
-    private float speed = 1f;
-
-    private Animator anim;
-
+    private float speed = 1f; // 플레이어 속도
+    private Animator anim; // 플레이어 에니메이션
     private Vector3 movDir;
+    private GameObject weaponObject; // 무기 교체
 
     private void Awake()
     {
@@ -26,7 +26,7 @@ public class PlayerCtrl : MonoBehaviour
         Walk();
     }
 
-    void Walk()
+    private void Walk() // 플레이어 에니메이션
     {
         movDir = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
         if (movDir != Vector3.zero) //Input.GetKey(KeyCode.W) 시간되면 뒤로가는 애니메이션 분리
@@ -50,11 +50,23 @@ public class PlayerCtrl : MonoBehaviour
         }
     }
 
-    void Move()
+    private void Move() //플레이어 이동
     {
         transform.Translate(Vector3.forward * Input.GetAxis("Vertical") * speed * Time.deltaTime);
         transform.Translate(Vector3.right * Input.GetAxis("Horizontal") * speed * Time.deltaTime);
     }
-    
-    
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (CompareTag("Weapon"))
+        {
+            weaponObject = other.gameObject;
+            Debug.Log(weaponObject.name);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        
+    }
 }

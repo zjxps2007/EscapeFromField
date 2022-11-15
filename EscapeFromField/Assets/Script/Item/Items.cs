326b857579bcd03569b06e51aefbf5a58b725b3b
joinho <zjxps2007@gmail.com>
2022-11-15 20:31:16 +0900

    Add to Inventory

---
+++ EscapeFromField/Assets/Script/Item/Items.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(fileName = "newItem", menuName = "newItem/item")]
+public class Items : ScriptableObject
+{
+    public string itemName; //아이템 이름
+    
+    public Sprite itemImage; //아이템 이미지
+
+    public GameObject itemPrefab; //아이템 프리팹
+
+    public string weaponType; //무기 타입
+
+    public ItemType itemType; //아이템 타입
+    
+    public enum ItemType
+    {
+        Equipment,
+        Used,
+        Ingredient,
+        ETC
+    }
+}

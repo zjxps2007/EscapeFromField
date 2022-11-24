6b8334870733956f585dcca5d58611c9ed7c6472
zjxps2007 <zjxps2007@gmail.com>
2022-11-24 16:08:11 +0900

    End

--- EscapeFromField/Assets/Script/UI/QuitMeue.cs
+++ EscapeFromField/Assets/Script/UI/QuitMeue.cs
@@ -15,10 +15,10 @@ public class QuitMeue : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Escape))
-        {
-            mene.gameObject.SetActive(true);
-            
-        }
+        // if (Input.GetKeyDown(KeyCode.Escape))
+        // {
+        //     mene.gameObject.SetActive(true);
+        //     
+        // }
     }
 }
